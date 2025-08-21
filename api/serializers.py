dea6e5f32b098320e7ea33d343d3502f86d04413
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-21 18:33:38 +0300

    created serializers

---
+++ api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Book, Review
+from taggit.serializers import TagListSerializerField
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class BookSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    tags = TagListSerializerField()
+    progress_percentage = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = Book
+        fields = '__all__'
+        read_only_fields = ['user', 'date_added', 'last_updated']
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ['user', 'created_at', 'updated_at']

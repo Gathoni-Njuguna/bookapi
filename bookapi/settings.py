5e8dc9afd994ff0278785a143ca64cdaecefbfa7
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-21 20:19:07 +0300

    update readme

--- bookapi/settings.py
+++ bookapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@er^)(o05gs5lx9#tq^c)tie+ot8neey0zy&bj+#u1k5)xzunq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bookapi-71m5.onrender.com', 'localhost', ]
 
 
 # Application definition

3d12d492bf561ac263f7e552a47ef8e17bc1e405
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-21 18:39:12 +0300

    created urls

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+# api/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'books', views.BookViewSet)
+router.register(r'reviews', views.ReviewViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
